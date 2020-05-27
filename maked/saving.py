6c86c50a457ce0e1af27cb38aeaa95391dbc0494
Niklas Sturm <>
2020-05-27 21:51:06 +0200

    Added authors table.

--- maked/saving.py
+++ maked/saving.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from queue import Queue
 from sqlite3 import Cursor
 from threading import Condition, Event
+from typing import Union, List, Dict
 
 import aiofiles
 
@@ -13,7 +14,6 @@ def save_to_files(
     queue: Queue, condition: Condition, finished_download_event: Event, base_path: Path
 ) -> None:
     base_path.mkdir(parents=True, exist_ok=True)
-
     asyncio.run(
         save_and_write_to_files(queue, condition, finished_download_event, base_path)
     )
@@ -62,8 +62,11 @@ def save_to_db(
             finally:
                 queue.task_done()
 
-        insert_aptnote(cursor, **aptnote)
-        insert_document(cursor, **document)
+        insert_aptnote(cursor, aptnote)
+        authors = document["author"]
+        del document["author"]
+        insert_document(cursor, document)
+        insert_authors(cursor, authors, document["unique_id"], aptnote.get("title"))
         connection.commit()
         logging.debug("Inserted values into db")
 
@@ -77,11 +80,13 @@ def setup_db(cursor: Cursor, drop: bool = False) -> None:
     """
     is_there_aptnotes = check_table(cursor, "aptnotes")
     is_there_documents = check_table(cursor, "documents")
+    is_there_authors = check_table(cursor, "authors")
 
-    if not is_there_aptnotes or not is_there_documents:
+    if not is_there_aptnotes and not is_there_documents and not is_there_authors:
         logging.debug("Tables not present. Creating them...")
         create_aptnotes_table(cursor)
         create_documents_table(cursor)
+        create_authors_tables(cursor)
     else:
         if is_there_aptnotes:
             if drop == True:
@@ -93,6 +98,11 @@ def setup_db(cursor: Cursor, drop: bool = False) -> None:
                 logging.debug("Dropping documents table...")
                 cursor.execute("DROP TABLE documents")
                 create_documents_table(cursor)
+        if is_there_authors:
+            if drop == True:
+                logging.debug("Dropping authors table...")
+                cursor.execute("DROP TABLE authors")
+                create_authors_tables(cursor)
 
 
 def check_table(cursor: Cursor, tablename: str) -> bool:
@@ -121,36 +131,54 @@ def create_documents_table(cursor: Cursor) -> None:
     cursor.execute(
         """
         CREATE TABLE documents
-        (id integer, fulltext text, author text, creation_date datetime, creator_tool text, creator_title text)
+        (id integer, fulltext text, creation_date datetime, creator_tool text, creator_title text)
         """
     )
 
 
-def insert_aptnote(
-    cursor, unique_id, filename, title, source, splash_url, sha1, date, file_url,
-) -> None:
+def create_authors_tables(cursor: Cursor) -> None:
     cursor.execute(
-        "INSERT INTO aptnotes VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-        (unique_id, filename, title, source, splash_url, sha1, date, file_url),
+        """
+        CREATE TABLE authors
+        (name text, document_id integer, title text)
+        """
     )
 
 
-def insert_document(
-    cursor, unique_id, fulltext, author, creation_date, creator_tool, creator_title
+def insert_aptnote(cursor: Cursor, parameters: Dict) -> None:
+    cursor.execute(
+        "INSERT INTO aptnotes VALUES (:unique_id, :filename, :title, :source, :splash_url, :sha1, :date, :file_url)",
+        parameters,
+    )
+
+
+def insert_document(cursor: Cursor, parameters: Dict) -> None:
+    cursor.execute(
+        "INSERT INTO documents VALUES (:unique_id, :fulltext, :creation_date, :creator_tool, :creator_title)",
+        parameters,
+    )
+
+
+def insert_authors(
+    cursor: Cursor, authors: Union[List, str], document_id: int, title: str
 ) -> None:
-    parameters = {
-        "id": unique_id,
-        "fulltext": fulltext,
-        "author": author,
-        "creation_date": creation_date,
-        "creator_tool": creator_tool,
-        "creator_title": creator_title,
+    author_row = {
+        "name": None,
+        "document_id": document_id,
+        "title": title,
     }
-    try:
-        cursor.execute(
-            "INSERT INTO documents VALUES (:id, :fulltext, :author, :creation_date, :creator_tool, :creator_title)",
-            parameters,
-        )
-    except sqlite3.InterfaceError as e:
-        logging.error("Error in interfaces", exc_info=e)
-        logging.error(f"Faulty parameter {parameters}")
+    if isinstance(authors, list):
+        for author in authors:
+            author_row["name"] = author
+            insert_author(cursor, author_row)
+    elif not authors or isinstance(authors, str):
+        insert_author(cursor, author_row)
+
+
+def insert_author(cursor: Cursor, parameters: Dict) -> None:
+    cursor.execute(
+        """
+        INSERT INTO authors VALUES (:name, :document_id, :title)
+        """,
+        parameters,
+    )
